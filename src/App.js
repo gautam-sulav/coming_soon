b78aa82260097396cdcac8adf8d27581f16be360
ano <ano@ano.ano>
2023-04-29 09:24:27 +0545

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span class="bar">
+        <i class="fa fa-bars"></i>
+      </span>
+      <nav class="toggle-nav">
+        <ul class="listing">
+          <li>
+            <a href="https://www.facebook.com/sulav.ano" target="_blank">
+              <i class="fa fa-facebook"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fa fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fa fa-instagram"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="#"></i>
+            </a>
+          </li>
+          <li>
+            <a href="mailto:sulavgautam@proton.me">
+              <i class="fa fa-envelope"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <section class="coming-soon">
+        <div class="coming-soon-inner">
+          <h1 class="heading">Coming Soon</h1>
+          <h2 class="small-heading">Under Construction</h2>
+          <div class="counter-timer">
+            <ul>
+              <li>
+                <span id="days"></span>days
+              </li>
+              <li>
+                <span id="hours"></span>Hours
+              </li>
+              <li>
+                <span id="minutes"></span>Minutes
+              </li>
+              <li>
+                <span id="seconds"></span>Seconds
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
-
 export default App;
